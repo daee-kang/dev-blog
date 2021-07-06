5a933bd7981e56cbe917a19b85be2c9365942693
daee kang <daee.kang@unlv.edu>
2021-07-06 12:31:01 -0700

    add cursor to button hover

--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -199,6 +199,7 @@ export default function PostLayout({
               box-shadow: 0px 2px 7px -2px rgba(0,0,0,0.51);
               transition:all 0.2s ease-in-out;
               z-index: 100;
+              cursor: pointer;
             }
             .jumpback:hover {
               -webkit-box-shadow: 0px 2px 12px -2px rgba(0,0,0,0.51);
